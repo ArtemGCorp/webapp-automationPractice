cd26b9ba7f2cdd8748f6537bc98d84bbc432faa1
Artiom Copusciu <acopusciu@griddynamics.com>
2026-05-18 14:58:30 +0300

    Deleted Cypress folder completely

--- playwright-tests/data/users.js
+++ playwright-tests/data/users.js
@@ -1,4 +1,7 @@
-export const testUser = { username: 'Katharina_Bernier', password: 's3cret', id: 't45AiwidW' };
+export const testUser = { 
+  username: 'Katharina_Bernier', 
+  password: 's3cret', 
+  id: 't45AiwidW' };
 export const nonExistingUser = {
   username: 'invalid_username',
   password: 'invalid_password',
