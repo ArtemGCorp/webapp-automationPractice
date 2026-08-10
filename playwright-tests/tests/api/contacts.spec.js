6658d4f0e168a0eed9062cb00eee8cf2eeead9dd
Artiom Copusciu <acopusciu@griddynamics.com>
2026-08-10 16:53:42 +0300

    moved testing framework to another place

--- playwright-tests/tests/api/contacts.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('../../utilities/fixtures.js');
-
-
-test('should get contacts', async ({ authRequest }) => {
-  const contactsResponse = await authRequest.get('/transactions/contacts');
-  let data = await contactsResponse.json();
-  expect(contactsResponse.ok()).toBeTruthy();
-  expect(data.results).toBeDefined();
-});
